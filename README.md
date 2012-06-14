053794c879d1c39827e47e83e7cbba57d30cafaa
Chris J. Davis <cjd@chrisjdavis.org>
2012-06-14 15:19:44 -0500

    A number of updates
    For the read me as well as the plugin itself.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 Section
-=======
+----------------------------------------------------
 
-Configure Habari to display content in a section, like /blog or /work.
+Configure Habari to display content in a section, like `/blog` or `/work`.
+
+Installation
+============
+
+1. Clone Section to your server: `git clone https://github.com/leagueofbeards/Section.git section`.
+1. Update the `URL_SPACE` constant to reflect whatever you are calling your section.
+1. Profit.
--- section.plugin.php
+++ section.plugin.php
@@ -1,33 +1,36 @@
 <?php
 class Section extends Plugin
 {
+	const URL_SPACE = 'blog';
+	
 	public function filter_default_rewrite_rules( $rules ) {
 		$rules[] = array(
-				'name' => 'display_multiple', 
-				'parse_regex' => '%^blog(/page/(?P<page>\d+))?/?$%i',
-				'build_str' => 'blog(/page/{$page})', 
+				'name' => 'display_blog_multiple', 
+				'parse_regex' => '%^' . self::URL_SPACE . '(/page/(?P<page>\d+))?/?$%i',
+				'build_str' => self::URL_SPACE . '(/page/{$page})', 
 				'handler' => 'UserThemeHandler', 
-				'action' => 'display_multiple', 
+				'action' => 'display_blog_multiple', 
 				'priority' => 5, 
-				'description' => 'Return blog type entries.', 
+				'description' => 'Create a section', 
 		);
 
 		$rules[] = array(
 			'name'			=>	'display_single',
-			'parse_regex'	=> '%^blog/(?P<slug>[^/]*)?/?$%i',
-			'build_str'		=> 'blog/{$slug}',
+			'parse_regex'	=> '%^' . self::URL_SPACE . '/(?P<slug>[^/]*)?/?$%i',
+			'build_str'		=> self::URL_SPACE . '/{$slug}',
 			'handler'		=>	'UserThemeHandler',
 			'action'		=>	'display_single',
 			'priority'		=>	2,
-			'description'	=>	'Display a given blog entry',
+			'description'	=>	'Display a entries from a section',
 		);
+		
 		return $rules;
 	}
 	
 	public function filter_theme_act_display_multiple( $handled, $theme ) {
 		$paramarray = array();
 		$paramarray['fallback'] = array(
-			'blog.multiple'
+			'section.multiple'
 		);
 
 		$default_filters = array(
@@ -42,7 +45,7 @@ class Section extends Plugin
 	public function filter_theme_act_display_single( $handled, $theme ) {
 		$paramarray = array();
 		$paramarray['fallback'] = array(
-			'blog.single'
+			'section.single'
 		);
 
 		$paramarray[ 'user_filters' ] = $default_filters;
